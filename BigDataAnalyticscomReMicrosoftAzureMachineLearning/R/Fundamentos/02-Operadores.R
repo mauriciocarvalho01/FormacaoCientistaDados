340726e74ffa5a91130cef2666cc1acc93376251
Mauricio Carvalho <developer.mauricio1@gmail.com>
2023-03-19 18:11:11 -0300

    estudo básico de tipos de dados

---
+++ BigDataAnalyticscomReMicrosoftAzureMachineLearning/R/Fundamentos/02-Operadores.R
@@ -0,0 +1,35 @@
+# Operadores Básicos Relacionais e Lógicos em R
+
+# Operadores básicos
+
+# Soma
+4+4
+# Subtração
+4/4
+# Potência
+4^2**2
+# Módulo
+14%%3
+
+
+# Opreadores relacionais
+
+# Atributos variáveis
+a = 7
+b = 8
+
+
+# Operadores
+a > 8
+b < 8
+a <= 8
+b >= 8
+a != 8
+
+#Operadores lógicos
+
+(a == 8) & (b != 6)
+(a == 8) | (b != 6)
+
+print(a != 8)
+
